f1c17cc16a4998e3f39a383f61acb977bd4d1ac0
obede123321 <obede.silva3@gmail.com>
2025-09-03 08:33:27 -0300

    exibindo variáveis no HTML

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const app = express();
 app.set("view engine",'ejs');
 
 
-app.get('/', (req, res) => {
-  // res.send('Bem-vindo ao GuiaPerguntas!');
-  res.render("index");
-});
+//app.get("/:nome/:lang",(req, res) => {
+  app.get("/",(req, res) => {
+    //res.send("Bem vindo ao GuiaPerguntas!");
+    
+    res.render("principal/perfil");
 
-app.listen(8080, () => {
-  console.log("Servidor rodando na porta");});
+  //var nome = req.params.nome;
+  //var lang = req.params.lang;
+ 
+  // res.send('Bem-vindo ao GuiaPerguntas!');
+  //res.render("index",{
+   //nome: nome, 
+   // lang: lang, 
+   // empresa: "Guia do programador", 
+   // inscritos: 8000 });
+  }); 
+app.listen(8080,()=>{console.log("Servidor rodando na porta");}); 
